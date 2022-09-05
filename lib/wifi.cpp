86ecc8b6066df9cf90ef9cd2171613e92280e809
hesham-salem <hesham1salem@gmail.com>
2022-09-05 11:54:12 +0200

    first

---
+++ lib/wifi.cpp
@@ -0,0 +1,5 @@
+#include "wifi.hpp"
+void init()
+{
+    cout << "from wifi" << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "calc.hpp"
+#include "wifi.hpp"
+using namespace std;
+int main()
+{
+    init();
+    // cout << add(5, 5) << endl;
+    return 0;
+}
